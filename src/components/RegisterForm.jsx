9c8e697c72de6424ec0a87a42f517926173c9875
MiroMuro <miro.laukkanen44@gmail.com>
2024-07-02 12:47:31 +0300

    Sharpened error handling and animations on registration page

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 const RegisterForm = () => {
   const navigate = useNavigate();
   const [isAnimating, setIsAnimating] = useState(false);
-
+  const [isProcessing, setIsProcessing] = useState(false);
   const [message, setMessage] = useState({
     text: "Register here!",
     style: "py-2 text-center bg-red-200 bg rounded mb-2",
@@ -19,6 +19,31 @@ const RegisterForm = () => {
   });
   const [repeatPassword, setRepeatPassword] = useState("");
 
+  const handleError = (error) => {
+    const { code, extensions, message } = error.networkError;
+    /*console.log("CODE: ", code);
+    console.log("EXTENSIONS: ", extensions);
+    console.log("MESSAGE: ", message);
+    console.log("PUrettu error, ", { error });
+    */
+    if (code === "DUPLICATE_USERNAME") {
+      setMessage({
+        text: extensions.message,
+        style: `bg-red-500 py-2 text-center bg rounded mb-2 `,
+      });
+    } else if (code === "NETWORK_ERROR") {
+      setMessage({
+        text: message,
+        style: `bg-red-500 py-2 text-center bg rounded mb-2 `,
+      });
+    }
+    setTimeout(() => {
+      setMessage({
+        text: "Register here!",
+        style: "py-2 text-center bg-red-200 bg rounded mb-2",
+      });
+    }, 10000);
+  };
   const triggerAnimation = () => {
     setIsAnimating(true);
     setTimeout(() => setIsAnimating(false), 1000); // Reset the state after 1 second
@@ -27,24 +52,30 @@ const RegisterForm = () => {
   //Handle the registration mutation.
   const [registration, result] = useMutation(REGISTER, {
     onError: (error) => {
+      /*const { code, extensions, message } = error.networkError;
+      console.log("CODE: ", code);
+      console.log("EXTENSIONS: ", extensions);
+      console.log("MESSAGE: ", message);
+      console.log("PUrettu error, ", { error });
+
       console.log(
         error.graphQLErrors[0].message +
           "code: " +
           error.graphQLErrors[0].extensions.code +
           "error: " +
           error.graphQLErrors[0].extensions.invalidArgs
-      );
-      triggerAnimation();
-      setMessage({
-        text: "Registration failed! " + error.graphQLErrors[0].extensions.code,
-        style: `bg-red-500 py-2 text-center bg rounded mb-2 `,
-      });
+      );*/
+      setTimeout(() => {
+        triggerAnimation();
+        handleError(error);
+        setIsProcessing(false);
+      }, 1000);
     },
     onCompleted: () => {
+      setIsProcessing(false);
       triggerAnimation();
-
       setMessage({
-        text: "Registration successful! Redirecting...",
+        text: "Registration successful! Redirecting to login...",
         style: `bg-green-500 py-2 text-center bg rounded mb-2 `,
       });
     },
@@ -88,6 +119,8 @@ const RegisterForm = () => {
   };
   //Handle the registration form submission
   const handleRegistration = async (event) => {
+    setIsProcessing(true);
+
     event.preventDefault();
     registration({
       variables: {
@@ -120,17 +153,50 @@ const RegisterForm = () => {
       </button>
     );
   };
+  const InfoBox = ({ isAnimating, isProcessing, message }) => {
+    return (
+      <div
+        className={`${message.style} ${
+          isAnimating ? "animate-scaleUpAndDown" : ""
+        }`}
+      >
+        {isProcessing ? (
+          <div className="flex items-center justify-center">
+            <svg
+              className="animate-spin h-6 w-6 mx-4 text-red-400 fill-red-600"
+              viewBox="0 0 101 101"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          </div>
+        ) : (
+          <h2 className={`${message.style}`}>{message.text}</h2>
+        )}
+      </div>
+    );
+  };
   return (
     <div className="flex">
       <div className="flex flex-col justify-end basis-28">
-        <div
+        {/*<div
           className={` ${message.style} ${
             isAnimating ? "animate-scaleUpAndDown" : ""
           }`}
         >
           <h2 className={`${message.style}`}>{message.text}</h2>{" "}
-        </div>
-
+        </div>*/}
+        <InfoBox
+          isAnimating={isAnimating}
+          isProcessing={isProcessing}
+          message={message}
+        />
         <form
           className="border-black border-2 rounded-md"
           onSubmit={handleRegistration}
