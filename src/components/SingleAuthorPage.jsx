0d7f73f90a1dc7f65db57fa60136e1e48038da2e
MiroMuro <miro.laukkanen44@gmail.com>
2024-08-20 12:50:10 +0300

    Added book listing to singleauthorpage

--- src/components/SingleAuthorPage.jsx
+++ src/components/SingleAuthorPage.jsx
@@ -1,6 +1,6 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
-import { GET_SINGLE_AUTHOR } from "./queries";
+import { GET_SINGLE_AUTHOR, ALL_BOOKS } from "./queries";
 import image from "../static/images/book.jpg";
 const SingleAuthorPage = () => {
   const { authorId } = useParams();
@@ -8,10 +8,35 @@ const SingleAuthorPage = () => {
   const { loading, error, data } = useQuery(GET_SINGLE_AUTHOR, {
     variables: { authorId },
   });
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   const author = data.getAuthorById;
+
+  const AuthorsBookList = ({ authorName }) => {
+    console.log("The author name is", authorName);
+    const { loading, error, data } = useQuery(ALL_BOOKS, {
+      variables: { author: authorName },
+    });
+    console.log("The data is", data);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    return (
+      <div className="flex flex-col">
+        {data.allBooks.map((book) => (
+          <Link to={"/book/" + book.id}>
+            <div>
+              <p className="text-2xl m-4">{book.title}</p>
+              <p className="text-2xl m-4">{book.published}</p>
+              <p className="text-2xl m-4">{book.genres.join(", ")}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+    );
+  };
+
   console.log(author);
   return (
     <div className="w-4/12 my-2 border-gray-400 border-2">
@@ -28,7 +53,7 @@ const SingleAuthorPage = () => {
             alt="Author"
           ></img>
         </div>
-        <div className="my-4">
+        <div className="my-4 border-2 border-black rounded-md bg-gray-200 w-6/12">
           <p className="text-3xl mb-6">Born: {author.born}</p>
           <p className="text-3xl mb-6">Books: {author.bookCount}</p>
           <p className="text-3xl mb-6">Nationality: </p>
@@ -43,7 +68,9 @@ const SingleAuthorPage = () => {
       </div>
       <div>
         <p className="text-3xl m-4">Books by this author:</p>
-        <div></div>
+        <div>
+          <AuthorsBookList authorName={author.name} />
+        </div>
       </div>
     </div>
   );
